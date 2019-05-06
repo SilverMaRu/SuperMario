f9618f89d86b66f26c4a71c4b00e9c7546a40ab5
SilverMaRu <setsuna00@qq.com>
2019-05-06 19:15:10 +0800

    更新项目20190506191415
    修改马里奥移动操作,使用物理材质控制空中/地面的摩擦

--- Assets/Scripts/Mario.cs
+++ Assets/Scripts/Mario.cs
@@ -21,26 +21,31 @@ public class Mario : MonoBehaviour
     private Rigidbody2D rgBody2D;
     private Animator animator;
     private MyPhysics myPhysics;
-    
+
     private float inputTime = 0;
-    private float skyInputTime = 0;
     private Vector2 velocity = Vector2.zero;
+
+    private PhysicsMaterial2D noFriction;
+    private PhysicsMaterial2D groundFriction;
 
     // Start is called before the first frame update
     void Start()
     {
         rgBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        myPhysics = GetComponent<MyPhysics>();
+        //myPhysics = GetComponent<MyPhysics>();
+
+        noFriction = Resources.Load<PhysicsMaterial2D>("PhysicMaterials/NoFrictionPhysicsMaterial2D");
+        groundFriction = Resources.Load<PhysicsMaterial2D>("PhysicMaterials/0.02FrictionPhysicsMaterial2D");
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity = myPhysics.velocity;
-        //inGround = velocity.y == 0;
+        //velocity = myPhysics.velocity;
+        velocity = rgBody2D.velocity;
         float h = Input.GetAxis("Horizontal");
-            float inputDirection = h / Mathf.Abs(h);
+        float inputDirection = h / Mathf.Abs(h);
         if (h != 0)
         {
             if (inputDirection * velocity.x < 0 || velocity.x == 0)
@@ -49,7 +54,8 @@ public class Mario : MonoBehaviour
             }
             inputTime = Mathf.Min(inputTime + Time.deltaTime * 1f, 10f);
             float finalForce = 0;
-            if (!myPhysics.onSky)
+            //if (!myPhysics.onSky)
+            if (velocity.y == 0)
             {
                 if (h > 0)
                 {
@@ -59,7 +65,11 @@ public class Mario : MonoBehaviour
                 {
                     transform.rotation = Quaternion.Euler(0, 180, 0);
                 }
-                finalForce = Mathf.Max(inGroundMoveForce * (Mathf.Clamp(1 / inputTime, 1f, 2f) - 1f), myPhysics.friction);
+                //finalForce = Mathf.Max(inGroundMoveForce * (Mathf.Clamp(1 / inputTime, 1f, 2f) - 1f), myPhysics.friction);
+                finalForce = Mathf.Max(inGroundMoveForce * (Mathf.Clamp(1 / inputTime, 1f, 2f) - 1f), 8.7f);
+                
+                //finalForce = rgBody2D.sharedMaterial.friction*2 + 10;
+                //finalForce = inGroundMoveForce * (Mathf.Clamp(1 / inputTime, 1f, 2f) - 1f, rgBody2D.sharedMaterial.friction);
                 //finalForce = inGroundMoveForce;
             }
             else
@@ -67,24 +77,40 @@ public class Mario : MonoBehaviour
                 finalForce = onSkyMoveForce * (Mathf.Clamp(1 / inputTime, 1f, 2f) - 1f);
                 //finalForce = onSkyMoveForce;
             }
-            velocity.x += inputDirection * finalForce * Time.deltaTime;
+            //velocity.x += inputDirection * finalForce * Time.deltaTime;
+            rgBody2D.AddForce(Vector2.right * finalForce * inputDirection);
         }
 
-        if (!myPhysics.onSky && Input.GetKeyDown(KeyCode.Space))
+        //if (!myPhysics.onSky && Input.GetKeyDown(KeyCode.Space))
+        if (velocity.y == 0 && Input.GetKeyDown(KeyCode.Space))
         {
             velocity.y += jumpForce;
+            rgBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        if (myPhysics.onSky && Input.GetKeyUp(KeyCode.Space) && velocity.y > 0)
+        //if (myPhysics.onSky && Input.GetKeyUp(KeyCode.Space) && velocity.y > 0)
+        if (velocity.y != 0 && Input.GetKeyUp(KeyCode.Space) && velocity.y > 0)
         {
-            velocity.y *= 0.25f;
+            //velocity.y *= 0.25f;
+            rgBody2D.velocity = Vector2.right * rgBody2D.velocity.x + Vector2.up * rgBody2D.velocity.y * 0.25f;
         }
 
-        myPhysics.velocity = velocity;
+        //myPhysics.velocity = velocity;
+        //rgBody2D.velocity = velocity;
 
         animator.SetFloat("speed_x", rgBody2D.velocity.x);
         animator.SetFloat("speed_y", rgBody2D.velocity.y);
         animator.SetFloat("force_x", h);
-        animator.SetBool("inGround", !myPhysics.onSky);
+        //animator.SetBool("inGround", !myPhysics.onSky);
+        animator.SetBool("inGround", rgBody2D.velocity.y == 0);
+        if(rgBody2D.velocity.y == 0)
+        {
+            rgBody2D.sharedMaterial = groundFriction;
+        }
+        else
+        {
+            rgBody2D.sharedMaterial = noFriction;
+
+        }
     }
 }
